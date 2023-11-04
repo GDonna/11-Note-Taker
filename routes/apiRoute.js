b17f9e2dd91f4529a00b18c9244bd5380be8342a
Donna Goon <g.donna120@gmail.com>
2023-11-04 13:21:29 -0500

    added routes

---
+++ routes/apiRoute.js
@@ -0,0 +1,62 @@
+
+const router = require("express").Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+
+
+router.get("/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
+  });
+});
+
+router.post("/notes", (req, res) => {
+  console.log(req.body);
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    jsonData.push(req.body);
+
+    fs.writeFile("./db/db.json", JSON.stringify(jsonData), (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Note written!");
+        res.json("New note added!");
+      }
+    });
+  });
+});
+
+router.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const jsonData = JSON.parse(data).filter((obj) => obj.id !== id);
+
+    fs.writeFile("./db/db.json", JSON.stringify(jsonData), (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Note deleted");
+      }
+    });
+  });
+  res.send("test note deleted");
+});
+
+module.exports = router;
